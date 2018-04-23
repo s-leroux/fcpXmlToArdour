3f285ecf72aa02a8d076364afa715e4350f387ec
Sylvain Leroux <sylvain@chicoree.fr>
2018-04-23 23:23:56 +0200

    Add sequence accessors

--- test/fcp5.js
+++ test/fcp5.js
@@ -16,12 +16,55 @@ describe("FCP5", function() {
     });
 
     it("should return a promise", function() {
-      return fs.readFileAsync("test/data/sample.ardour")
+      return fs.readFileAsync("test/data/sample.fcp5")
         .then((data) => {
           const p = FCP5.load(data);
           assert.property(p, 'then');
 
           return p;
+        });
+    });
+
+  });
+  describe("the sequences property", function() {
+
+    it("should return an array of Sequence", function() {
+      return fs.readFileAsync("test/data/sample.fcp5")
+        .then(FCP5.load)
+        .then((doc) => {
+          const sequences = doc.sequences;
+          assert(sequences instanceof Array);
+          sequences.forEach((item) => assert.equal(item.constructor.name, "Sequence"));
+        });
+    });
+
+    it("should map sequence by id and index", function() {
+      return fs.readFileAsync("test/data/sample.fcp5")
+        .then(FCP5.load)
+        .then((doc) => {
+          const sequences = doc.sequences;
+          sequences.forEach((item) => assert.equal(item, sequences[item.id]));
+        });
+    });
+
+  });
+  describe("Sequence instances", function() {
+
+    it("should have an id property", function() {
+      return fs.readFileAsync("test/data/sample.fcp5")
+        .then(FCP5.load)
+        .then((doc) => {
+          const sequences = doc.sequences;
+          assert(sequences[0].id, 'E30R21M9');
+        });
+    });
+
+    it("should have a name property", function() {
+      return fs.readFileAsync("test/data/sample.fcp5")
+        .then(FCP5.load)
+        .then((doc) => {
+          const sequences = doc.sequences;
+          assert(sequences[0].name, 'Sequence #1');
         });
     });
 
